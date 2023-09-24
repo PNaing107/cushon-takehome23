c2ba46ff3452237ddac2b7d41ce7397e4e39505f
Phone Naing <phone.naing.dev@gmail.com>
2023-09-24 19:45:10 +0100

    config: update settings

--- app/settings.php
+++ app/settings.php
@@ -19,7 +19,7 @@ return function (ContainerBuilder $containerBuilder) {
             ],
             'db' => [
                 'host' => 'mysql:host=127.0.0.1;',
-                'name' => 'dbname=example',
+                'name' => 'dbname=retail_customers',
                 'user' => 'root',
                 'password' => 'password',
                 'debug' => true
