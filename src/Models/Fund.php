fbd0b83cab88578283f1ac86fc6d0b86e1d3f58b
Phone Naing <phone.naing.dev@gmail.com>
2023-09-25 12:58:42 +0100

    feat: make Fund model

---
+++ src/Models/Fund.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Models;
+
+use App\DataAccess\Database;
+use DateTime;
+use JsonSerializable;
+
+class Fund implements JsonSerializable
+{
+    private int $id;
+    private string $symbol;
+    private string $name;
+    private float $net_asset_value;
+    private int $risk_factor;
+    private float $ongoing_charge;
+    private string $created_at;
+    private string $updated_at;
+    private ?string $deleted_at;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getSymbol(): string
+    {
+        return $this->symbol;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getNetAssetValue(): float
+    {
+        return $this->net_asset_value;
+    }
+
+    public function getRiskFactor(): int
+    {
+        return $this->risk_factor;
+    }
+
+    public function getOngoingCharge(): float
+    {
+        return $this->ongoing_charge;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return $this->updated_at;
+    }
+
+    public function getDeletedAt(): ?string
+    {
+        return $this->deleted_at;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'symbol' => $this->getSymbol(),
+            'name' => $this->getName(),
+            'net_asset_value' => $this->getNetAssetValue(),
+            'risk_factor' => $this->getRiskFactor(),
+            'ongoing_charge' => $this->getOngoingCharge(),
+            'created_at' => $this->getCreatedAt(),
+            'updated_at' => $this->getUpdatedAt()
+        ];
+    }
+}
