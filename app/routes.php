deacbf984082d66a74878df574864f3934d62f41
Phone Naing <phone.naing.dev@gmail.com>
2023-09-25 12:57:02 +0100

    feat: add a GET /funds route

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-use App\Controllers\CoursesAPIController;
+use App\Controllers\FundsController;
 use Slim\App;
 use Slim\Views\PhpRenderer;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
@@ -9,13 +9,19 @@ use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 return function (App $app) {
     $container = $app->getContainer();
 
-    //demo code - two ways of linking urls to functionality, either via anon function or linking to a controller
-
-    $app->get('/', function ($request, $response, $args) use ($container) {
+    /* $app->get('/', function ($request, $response, $args) use ($container) {
         $renderer = $container->get(PhpRenderer::class);
         return $renderer->render($response, "index.php", $args);
+    }); */
+
+    $app->get('/test', function ($request, $response, $args) use ($container) {
+        
+        return $response->withJson();
     });
 
-    $app->get('/courses', CoursesAPIController::class);
+    // This route could be public
+    $app->get('/funds', FundsController::class . ':index');
+
+    // Routes to create, update and delete Funds must be restricted to Admins only (out of scope for this task)
 
 };
