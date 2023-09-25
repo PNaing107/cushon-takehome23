bee4e4ba0031b177d639a599522e204f6c853e45
Phone Naing <phone.naing.dev@gmail.com>
2023-09-25 13:00:21 +0100

    feat: make FundsController

---
+++ src/Controllers/FundsController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Controllers;
+
+
+use App\Services\FundService;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class FundsController
+{
+    private FundService $service;
+
+    // Here, the parameter is automatically supplied by the Dependency Injection Container based on the type hint
+    public function __construct(FundService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(RequestInterface $request, ResponseInterface $response): ResponseInterface
+    {
+        $responseBody = $this->service->getFunds();
+
+        return $response->withJson($responseBody)->withStatus($responseBody['status']);
+    }
+}
