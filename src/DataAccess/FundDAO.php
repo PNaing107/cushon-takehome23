d6c9e6e966c3415d947d8d0053e09d39df0f40b8
Phone Naing <phone.naing.dev@gmail.com>
2023-09-25 12:59:36 +0100

    feat: make FundDAO

---
+++ src/DataAccess/FundDAO.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataAccess;
+
+use App\Models\Fund;
+use App\Models\ResourceCollection;
+
+class FundDAO
+{
+    public function getAll(): array
+    {
+        $sql = 'SELECT * FROM `funds` where `deleted_at` IS NULL;';
+
+        $result = Database::getInstance()->fetchAll($sql, [], [\PDO::FETCH_CLASS, Fund::class] );
+
+        
+        $resourceCollection = new ResourceCollection;
+        
+        $resourceCollection->setCollection($result);
+
+        return $result;
+    }
+}
