53f6d38cb8e60ba910cbb9b8cb4026ab7fdc6a76
Phone Naing <phone.naing.dev@gmail.com>
2023-09-26 11:43:38 +0100

    feat: make InvestmentTransactionController

---
+++ src/Controllers/InvestmentTransactionsController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Controllers;
+
+
+use App\Services\InvestmentTransactionService;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class InvestmentTransactionsController
+{
+    private InvestmentTransactionService $service;
+
+    // Here, the parameter is automatically supplied by the Dependency Injection Container based on the type hint
+    public function __construct(InvestmentTransactionService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index(RequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $accountUuid = $args['account_id'];
+
+        $responseBody = $this->service->getAll($accountUuid);
+
+        return $response->withJson($responseBody)->withStatus($responseBody['status']);
+    }
+
+    public function store(RequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $accountID = $args['account_id'];
+
+        $body = $request->getParsedBody();
+
+        $responseBody = $this->service->store($accountID, $body);
+
+        return $response->withJson($responseBody)->withStatus($responseBody['status']);
+    }
+}
