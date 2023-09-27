0556942d370b197ec52c4829c8118d2e5bac9d69
Phone Naing <phone.naing.dev@gmail.com>
2023-09-27 12:43:32 +0100

    feat: add method to calculate summary of shares owned

--- src/DataAccess/DAO/InvestmentTransactionDAO.php
+++ src/DataAccess/DAO/InvestmentTransactionDAO.php
@@ -19,7 +19,7 @@ class InvestmentTransactionDAO implements DataAccessor
                 `it`.`net_asset_value`,
                 `it`.`created_at`
                 FROM `investment_transactions` as `it`
-                LEFT JOIN `funds` as `f` on `f`.`id` = `it`.`fund_id` 
+                INNER JOIN `funds` as `f` on `f`.`id` = `it`.`fund_id` 
                 WHERE `it`.`account_id` = (SELECT `id` FROM `accounts` WHERE `investment_account_uuid` = :accountUuiD);';
 
         $result = Database::getInstance()->fetchAll($sql, [$accountUuid], [\PDO::FETCH_CLASS, InvestmentTransaction::class] );
@@ -31,6 +31,38 @@ class InvestmentTransactionDAO implements DataAccessor
     {
         //TODO
         return [];
+    }
+
+    public function getAggregateShares(string $accountUuid): array
+    {
+        $sql = 'SELECT 
+                `f`.`symbol` as `symbol`, 
+                SUM(`it`.`shares`) as `shares`
+                FROM `investment_transactions` as `it`
+                INNER JOIN `funds` as `f` on `f`.`id` = `it`.`fund_id`
+                WHERE `it`.`account_id` = (SELECT `id` FROM `accounts` WHERE `investment_account_uuid` = :accountUuid) 
+                GROUP BY `f`.`symbol`
+                HAVING `shares` > 0';
+
+        $result = Database::getInstance()->fetchAll($sql, [$accountUuid]);
+
+        return $result;
+    }
+
+    public function store(array $transactions, int $accountId)
+    {
+        // for now we only handle a single transaction per request
+        $fundId = $transactions[0]['id'];
+        $amount = $transactions[0]['amount'];
+        $netAssetValue = $transactions[0]['net_asset_value'];
+
+        $sql = 'INSERT INTO `investment_transactions` 
+                (`account_id`, `fund_id`, `amount`, `net_asset_value`) 
+                VALUES 
+                (:accountId, :fundId, :amount, :netAssetValue)';
+
+        $result = Database::getInstance()->fetch($sql, [$accountId, $fundId, $amount,  $netAssetValue]);
+
     }
 
     public function getModelBinding(): string
