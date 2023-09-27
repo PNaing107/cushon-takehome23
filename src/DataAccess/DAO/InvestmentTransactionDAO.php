44f3fc9fed1c5b2368d5b25d71547dbf42949416
Phone Naing <phone.naing.dev@gmail.com>
2023-09-27 22:11:50 +0100

    refactor: return response from database

--- src/DataAccess/DAO/InvestmentTransactionDAO.php
+++ src/DataAccess/DAO/InvestmentTransactionDAO.php
@@ -61,8 +61,7 @@ class InvestmentTransactionDAO implements DataAccessor
                 VALUES 
                 (:accountId, :fundId, :amount, :netAssetValue)';
 
-        Database::getInstance()->fetch($sql, [$accountId, $fundId, $amount,  $netAssetValue]);
-
+        return Database::getInstance()->fetch($sql, [$accountId, $fundId, $amount,  $netAssetValue]);
     }
 
     public function getModelBinding(): string
